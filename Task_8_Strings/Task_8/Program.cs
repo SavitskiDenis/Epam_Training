e880c95ed9915f9501db6c328954f7f40412dfd9
SavitskiDenis <random.ssd@yandex.by>
2018-10-17 19:30:23 +0300

    add Task_8_Strings

---
+++ Task_8_Strings/Task_8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordsRedaction first = new WordsRedaction("Я сразу смазал карту будня,плеснувши краску из стакана,я показал на блюде студня косые скулы океана.На чешуе жестяной рыбы прочёл я зовы новых губ.А вы ноктюрн сыграть могли бы на флейте водосточных труб?");
+            string[] _myArray = first.stringArray;
+            for (int i = 0; i < _myArray.Length; i++)
+            {
+                Console.WriteLine(_myArray[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_8_Strings/Task_8/WordsRedaction.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8
+{
+    class WordsRedaction
+    {
+        private  char[] _vowelArray = { 'а', 'у', 'е', 'ё', 'ы', 'о', 'я', 'и', 'э', 'ю' };
+        private char[] _badLeters = { 'р', 'л', 'м', 'н','й'};
+        private string [] _myStringArray;
+
+        public WordsRedaction(string _myString)
+        {
+            _myStringArray = _myString.Split(new char[] {' ','.',',','?'});
+            StartSplit();
+            
+        }
+
+        private void StartSplit()
+        {
+            for (int i = 0; i < _myStringArray.Length; i++)
+            {
+                _myStringArray[i] = SplitWord(_myStringArray[i]);
+               
+            }
+          
+        }
+
+        private  string SplitWord(string _myStringVar)
+       {
+           StringBuilder _myBuilder = new StringBuilder(_myStringVar);
+           bool _check=false;
+               
+           for (int i = 0; i < _myBuilder.Length; i++)
+           {
+               if(IsVowel(_myBuilder[i]) && i!=_myBuilder.Length-1)
+               {
+                   if (i + 1 == _myBuilder.Length - 1 && !IsVowel(_myBuilder[i + 1]))
+                   {
+                       continue;
+                   }
+                   else
+                   {
+                       if(i+2!=_myBuilder.Length-1 && !IsVowel(_myBuilder[i+1]))
+                       {
+                           for (int j = 0; j < _badLeters.Length; j++)
+                           {
+                               if(_myBuilder[i+1]==_badLeters[j] && !IsVowel(_myBuilder[i+2]))
+                               {
+                                   _check = true;
+                                   _myBuilder.Insert(i + 2, "-");
+                                   break;
+                               }
+                           }
+                           if(!_check)
+                           {
+                               _myBuilder.Insert(i + 1, "-");
+                           }
+                       }
+                       else
+                       {
+                           if(!IsVowel(_myBuilder[i+1]) && !IsVowel(_myBuilder[i+2]))
+                           {
+                               continue;
+                           }
+                           _myBuilder.Insert(i + 1, "-");
+                       }
+                       
+                   }
+                   
+               }
+               
+           }
+
+           return _myBuilder.ToString();
+       }
+
+        private bool IsVowel(char _myChar)
+        {
+            for (int i = 0; i < _vowelArray.Length; i++)
+            {
+                if(Char.ToUpper(_myChar)==Char.ToUpper(_vowelArray[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public string[] stringArray
+        {
+            get
+            {
+                if(_myStringArray!=null)
+                {
+                    return _myStringArray;
+                }
+                return null;
+            }
+        }
+    }
+}
